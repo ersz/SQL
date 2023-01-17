7a9f6ba0e218a50614b8e64919c24a15cc16f902
ersz <68855838+ersz@users.noreply.github.com>
2023-01-17 12:06:13 -0500

    Add files via upload
    Find all columns with a specific string

---
+++ Find specific column in all tables.sql
@@ -0,0 +1,12 @@
+USE [data]
+GO
+
+SELECT
+   sys.columns.name AS ColumnName,
+   tables.name AS TableName
+FROM
+  sys.columns
+JOIN sys.tables ON
+  sys.columns.object_id = tables.object_id
+WHERE
+ sys.columns.name like '%ord_no%' -- <- to find all tables, change sys.tables.name like '%oeord%'
