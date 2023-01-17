6aad2791bd3eaee9915d03d5f05f1042bdedcf2d
ersz <68855838+ersz@users.noreply.github.com>
2023-01-17 12:38:18 -0500

    Update Displaying the temporary table.sql
    Displaying the temporary table with open orders without tracking numbers

--- Displaying the temporary table.sql
+++ Displaying the temporary table.sql
@@ -6,5 +6,3 @@ SELECT (Select count(mfg_loc)
 					Where status = 4 and mfg_loc = 'HLH') as 'HLH total', *
 FROM dbo.[Missing Tracking Numbers] WHERE mfg_loc <> 'AC' and ship_to_name <> 'Help Task'
 Order by 2 DESC;
-
-
