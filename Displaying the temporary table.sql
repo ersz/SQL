1ee4d9a72b4f2f09071bceb409a74af548fd5fb9
ersz <68855838+ersz@users.noreply.github.com>
2023-01-17 12:37:01 -0500

    Update Displaying the temporary table.sql

--- Displaying the temporary table.sql
+++ Displaying the temporary table.sql
@@ -1,8 +1,8 @@
-USE [data_CC]	
+USE [data_ZZ]	
 GO
 
 SELECT (Select count(mfg_loc)
-				From [data_CC].[dbo].[oeor_sql]
+				From [data_ZZ].[dbo].[oeor_sql]
 					Where status = 4 and mfg_loc = 'HLH') as 'HLH total', *
 FROM dbo.[Missing Tracking Numbers] WHERE mfg_loc <> 'AC' and ship_to_name <> 'Help Task'
 Order by 2 DESC;
