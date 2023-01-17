0d6ee5548e7284488d79202facea44d9d5dc8149
ersz <68855838+ersz@users.noreply.github.com>
2023-01-17 12:35:59 -0500

    Add files via upload
    Displaying the temporary table with orders that don't have a tracking number

---
+++ Displaying the temporary table.sql
@@ -0,0 +1,10 @@
+USE [data_CC]	
+GO
+
+SELECT (Select count(mfg_loc)
+				From [data_CC].[dbo].[oeor_sql]
+					Where status = 4 and mfg_loc = 'HLH') as 'HLH total', *
+FROM dbo.[Missing Tracking Numbers] WHERE mfg_loc <> 'AC' and ship_to_name <> 'Help Task'
+Order by 2 DESC;
+
+
