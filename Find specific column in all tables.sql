3859def1d20c9d97fe52dd18982fdf727203de61
ersz <68855838+ersz@users.noreply.github.com>
2023-02-03 15:56:50 -0500

    Update Find specific column in all tables.sql

--- Find specific column in all tables.sql
+++ Find specific column in all tables.sql
@@ -9,4 +9,4 @@ FROM
 JOIN sys.tables ON
   sys.columns.object_id = tables.object_id
 WHERE
- sys.columns.name like '%ord_no%' -- <- to find all tables, change sys.tables.name like '%oeord%'
+ sys.columns.name like '%ord_no%' -- <- to find all tables, change sys.tables.name like '%oeord%';
