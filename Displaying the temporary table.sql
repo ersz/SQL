2a316b487b929a8ff810f9b15d0cd84ae89c44fb
ersz <68855838+ersz@users.noreply.github.com>
2023-02-03 15:55:28 -0500

    Update Displaying the temporary table.sql

--- Displaying the temporary table.sql
+++ Displaying the temporary table.sql
@@ -1,8 +1,13 @@
 USE [data_ZZ]	
 GO
 
-SELECT (Select count(mfg_loc)
-				From [data_ZZ].[dbo].[oeor_sql]
-					Where status = 4 and mfg_loc = 'HLH') as 'HLH total', *
-FROM dbo.[Missing Tracking Numbers] WHERE mfg_loc <> 'AC' and ship_to_name <> 'Help Task'
-Order by 2 DESC;
+SELECT
+	(Select count(mfg_voc)	
+	 From [data_ZZ].[dbo].[oeor_sql] 	
+	 Where status = 4 and mfg_voc = 'HLH') as 'HLH total', *
+FROM 
+	dbo.[Missing Tracking Numbers]
+WHERE
+	mfg_voc <> 'AC' and ship_to_name <> 'Help Task'
+Order by
+	2 DESC;
